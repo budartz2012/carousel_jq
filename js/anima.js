4ca87dfe40817c1108a85349f451eeebdee99009
Daniel S <budartz@gmail.com>
2017-12-08 15:48:26 +0000

    Fading done
    resolved the fading in and out for both next and prev slides by setting out side the fading function the nextActiveSlide to fadeOut() then inside the fade func setted first the currentSlide to fadeOut and then inside the function setted the "callback" to fadeIn the nextSlide -(previously fadeOut. This allowed that the fadeIn to work in the first cycle - cos was already fadeOut if not only it would only fadeIn in the second cycle

--- js/anima.js
+++ js/anima.js
@@ -1,35 +1,44 @@
 
 $(document).ready(function(){
 	$(".next").on("click", function (){
-    
+
 		let currentActiveSlide = $(".active");
-		let nextActiveSlide = currentActiveSlide.next();
+		let nextActiveSlide = currentActiveSlide.next().fadeOut();
+
+			currentActiveSlide.fadeOut(1000, function(){
+			nextActiveSlide.fadeIn(500)
+			});
 
 		if(nextActiveSlide.length==0){
 			nextActiveSlide = $(".carousel-inner img").first();
 		};
 		if (nextActiveSlide.length){
-		currentActiveSlide.removeClass("active").css("z-index", -10);
-		nextActiveSlide.addClass("active").css("z-index", 10);
-
-	    };
+			currentActiveSlide.removeClass("active");
+			nextActiveSlide.addClass("active");
+		};
+			
+			
+   
 	});
 
-		
+
 
 	$(".prev").on("click", function (){
 
-     	
+	    let currentActiveSlide = $(".active");
+		let prevActiveSlide = currentActiveSlide.prev().fadeOut();
 
-		let currentActiveSlide = $(".active");
-		let prevActiveSlide = currentActiveSlide.prev();
+			currentActiveSlide.fadeOut(2000, function(){
+			prevActiveSlide.fadeIn(1000)
+			});
 
+			
 		if(prevActiveSlide.length==0){
 			prevActiveSlide = $(".carousel-inner img").last();
 		};
 		if (prevActiveSlide.length){
-		currentActiveSlide.removeClass("active").css("z-index", -10);
-		prevActiveSlide.addClass("active").css("z-index", 10);
+			currentActiveSlide.removeClass("active");
+			prevActiveSlide.addClass("active");
 
 	    };
 	});
